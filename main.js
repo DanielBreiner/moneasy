6018b9933a26df0b4cb9b29de7a66e2d9ae34d37
itopencek <ivohavasi@gmail.com>
2019-03-30 12:56:02 +0100

    zaciatocne rozdelenie suborov

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-console.log("Hello world!");
-console.log("Testing automatic deploys on Heroku.");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+let path = require('path');
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//midleware for public/static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+app.use('/', require('./routes/routes'));
+
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+//port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started. Port: ${PORT}`));
+
