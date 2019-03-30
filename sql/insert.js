5c9dedfd09c72f93b34efe2053b17a96455d5f05
DanielBreiner <danielbreinerd@gmail.com>
2019-03-30 18:42:35 +0100

    ajax show expenses

--- sql/insert.js
+++ sql/insert.js
@@ -5,9 +5,9 @@ function main(c){
     return insert;
 }
 
-async function insert(table, data, callback){
+function insert(table, data, callback){
 
-    await client.connect();    
+    client.connect();    
     client.query(`INSERT INTO public.${table} VALUES ('${data["name"]}', ${data["amount"]}, '${data["note"]}', ${data["date"]}, '${data["category"]}');`, (err, res) => {
         if (err) throw err;
         callback(res.rowCount == 1);
