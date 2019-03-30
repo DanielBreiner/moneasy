58d143bd50e888878baefde557fe47774ea6310f
DanielBreiner <danielbreinerd@gmail.com>
2019-03-30 23:21:18 +0100

    installed body-parser

--- routes/routes.js
+++ routes/routes.js
@@ -6,43 +6,40 @@ router.get('/', (req, res) => {
     res.render('index')
 });
 
-router.put("/sql", (req, res) => {
+router.post("/sql", (req, res) => {
+    console.log(req.body);
     
-})
+    //NOTE(DanoB) replace true with check if user is logged in
+    // if (true && req.body.category && req.body.amount && req.body.note) {
+    //     let name = 'admin' //NOTE(DanoB) Replace when login is working
+    //     let category = req.body.category;
+    //     let amount = req.body.amount;
+    //     let note = req.body.note;
+    //     let date = new Date().getTime();
+
+    //     sql.insert(
+    //         "spending",
+    //         {
+    //             "name": name,
+    //             "category": category,
+    //             "amount": amount,
+    //             "note": note,
+    //             "date": date
+    //         },
+    //         function (succ) {
+    //             if (succ) {
+    //                 res.send(succ)
+    //             }
+    //         }
+    //     );
+    // }
+});
 
 router.get('/sql', (req, res) => {
-    //NOTE(DanoB) replace true with check if user is logged in
-    if (true && req.query.category && req.query.amount && req.query.note) {
-        let name = 'admin' //NOTE(DanoB) Replace when login is working
-        let category = req.query.category;
-        let amount = req.query.amount;
-        let note = req.query.note;
-        let date = new Date().getTime();
-
-        sql.insert(
-            "spending",
-            {
-                "name": name,
-                "category": category,
-                "amount": amount,
-                "note": note,
-                "date": date
-            },
-            function (succ) {
-                if (succ) {
-                    res.send("success")
-                }
-            }
-        );
-    } else {
-        sql.request("spending", function (data) {
-            console.log("success");
-
-            res.set('Content-Type', 'application/json');
-            res.send(data);
-        })
-
-    }
+    sql.request("spending", function (data) {
+        res.set('Content-Type', 'application/json');
+        res.send(data);
+    })
 });
 
 router.get('/dashboard', (req, res) => {
