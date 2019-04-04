fdb3fd23c8dae2416c1afd66fef05632647bb15d
DanielBreiner <danielbreinerd@gmail.com>
2019-04-04 20:46:32 +0200

    secret config keys

--- sql.js
+++ sql.js
@@ -1,8 +1,9 @@
 const { Pool } = require('pg');
+const keys = require("./config/keys")
 
 function connect() {
     return new Pool({
-        connectionString: process.env.DATABASE_URL || "postgres://ihzcdzutvpzmke:cd33226578fe2baf2a26655d13f65a1fc570ab4018a1d319ab61eb8321fc445f@ec2-54-247-85-251.eu-west-1.compute.amazonaws.com:5432/d4d1ci2ao6kktu",
+        connectionString: process.env.DATABASE_URL || keys.postgres.connectionString,
         ssl: true,
     });
 }
