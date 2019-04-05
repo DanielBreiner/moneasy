65fbc563c6e64e8aa631048db8b7447d2a33aa30
DanielBreiner <danielbreinerd@gmail.com>
2019-04-05 22:41:44 +0200

    SQL rework init

--- sql.js
+++ sql.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 const keys = require("./config/keys")
-
 function connect() {
     return new Pool({
         connectionString: process.env.DATABASE_URL || keys.postgres.connectionString,
@@ -8,11 +7,49 @@ function connect() {
     });
 }
 
+const { Client } = require('pg').Client;
+/**
+ * Main way to access the SQL database
+ * 
+ * @param {string} query SQL query
+ * @param {function} cb Result callback; 1 parameter: result
+ * @param {function} onErr Error callback; 1 parameter: error
+ */
+module.exports.query = function (query, cb, onErr) {
+    let client = new Client({
+        connectionString: process.env.DATABASE_URL || keys.postgres.connectionString,
+        ssl: true,
+    });
+    client.connect()
+    client.query(query) // your query string here
+        .then((res) => { cb(res); }) // your callback here
+        .catch((err) => { onErr(err); }) // your callback here
+        .then(() => { client.end(); })
+}
+
 module.exports = {
-    "connect": connect,
-    "insert": require("./sql/insert.js")(connect),
-    "request": require("./sql/request.js")(connect),
-    "requestAll": require("./sql/requestAll.js")(connect),
-    "requestRaw": require("./sql/requestRaw.js")(connect),
-    "insertGoal": require("./sql/insertGoal.js")(connect)
+    /**
+     * @deprecated
+     */
+    connect: connect,
+    /**
+     * @deprecated
+     */
+    insert: require("./sql/insert.js")(connect),
+    /**
+     * @deprecated
+     */
+    request: require("./sql/request.js")(connect),
+    /**
+     * @deprecated
+     */
+    requestAll: require("./sql/requestAll.js")(connect),
+    /**
+     * @deprecated
+     */
+    requestRaw: require("./sql/requestRaw.js")(connect),
+    /**
+     * @deprecated
+     */
+    insertGoal: require("./sql/insertGoal.js")(connect)
 }
